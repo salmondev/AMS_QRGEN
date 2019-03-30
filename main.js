5eca32791c55a736c7910ec12a02b3769e6cbf04
ARMY <panurut.army@hotmail.com>
2019-03-30 16:29:41 +0700

    Release commit

--- main.js
+++ main.js
@@ -1,52 +1,85 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  shell
+} = require("electron");
+const path = require("path");
+const fs = require("fs");
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+var mainWindow = null;
+var workerWindow = null;
 
-function createWindow () {
-  // Create the browser window.
+// シングルページのアプリとしてメインが閉じたら終了とする
+app.once("ready", () => {
   mainWindow = new BrowserWindow({
     width: 1250,
     height: 750,
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
+  mainWindow.loadURL("file://" + __dirname + "/main.html");
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.on("closed", () => {
+    // workerページの解放漏れが無いことを確認するためのリスト
+    // * この時点でmainページは含まれないので正常なら対象がない
+    console.log("window list =",
+      BrowserWindow.getAllWindows().map((win) => {
+        return win.id;
+      }));
+    app.quit();
+  });
+});
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+// mainページからの印刷要求
+ipcMain.on("print-to-pdf", (event, content) => {
+  if (workerWindow !== null) {
+    // エラーにより前回のページが残っていたら閉じる
+    workerWindow.close();
+  }
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
+  workerWindow = new BrowserWindow({
+    show: false
+  });
+  workerWindow.loadURL("file://" + __dirname + "/worker.html");
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+  workerWindow.on("closed", () => {
+    workerWindow = null;
+  });
+
+  // workerページが準備完了した後に要求を投げるようにする
+  workerWindow.on("ready-to-show", () => {
+    workerWindow.send("print-to-pdf", content);
+  });
+});
+
+// workerページからの準備完了通知
+ipcMain.on("ready-print-to-pdf", (event) => {
+  var random = Math.random();
+  var randomInt = Math.floor(Math.random() * 1000000);
+  //const pdfPath = path.join(app.getPath("temp"), "print" + i + ".pdf");
+  const pdfPath = path.join("printQR_" + randomInt + ".pdf");
+  const options = {
+    printBackground: true
+  };
+
+  workerWindow.webContents.printToPDF(options, (error, data) => {
+    if (error) {
+      throw error;
+    }
+    fs.writeFile(pdfPath, data, (error) => {
+      if (error) {
+        throw error;
+      }
+      // プレビュー表示
+      shell.openItem(pdfPath);
+      // PDFファイルは書き込み済みなのでworkerページは閉じても大丈夫
+      workerWindow.close();
+    });
+  });
+});
